4bd8c717f5f646aa18429f3328ba5e913c79370f
Alex Job <ajob@eidebailly.com>
2020-03-09 12:46:13 -0500

    Added IsNullOrEmpty extension method

--- src/ChoreBoard.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
+++ src/ChoreBoard.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using ChoreBoard.Utility.Extensions;
+using Xunit;
 
 namespace ChoreBoard.Utility.Tests.Extensions
 {
-    class EnumerableExtensionsTests
+    public class EnumerableExtensionsTests
     {
+        public class IsNullOrEmptyMethod
+        {
+            [Theory]
+            [InlineData(null)]
+            public void ReturnsTrueWhenPassedNullValue<T>(IEnumerable<T> enumerable)
+            {
+                bool result = enumerable.IsNullOrEmpty();
+
+                Assert.True(result);
+            }
+
+            [Fact]
+            public void ReturnsTrueForZeroLengthArray()
+            {
+                int[] array = new int[0];
+
+                bool result = array.IsNullOrEmpty();
+
+                Assert.True(result);
+            }
+
+            [Fact]
+            public void ReturnsTrueForEmptyCollection()
+            {
+                List<int> list = new List<int>(10);
+
+                bool result = list.IsNullOrEmpty();
+
+                Assert.True(result);
+            }
+
+            [Fact]
+            public void ReturnsFalseForNonZeroLengthArray()
+            {
+                int[] array = new int[1];
+
+                bool result = array.IsNullOrEmpty();
+
+                Assert.False(result);
+            }
+
+            [Fact]
+            public void ReturnsFalseWhenCollectionContainsElements()
+            {
+                List<int> list = new List<int> { 0 };
+
+                bool result = list.IsNullOrEmpty();
+
+                Assert.False(result);
+            }
+        }
     }
 }
--- src/ChoreBoard.Utility/Extensions/EnumerableExtensions.cs
+++ src/ChoreBoard.Utility/Extensions/EnumerableExtensions.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace ChoreBoard.Utility.Extensions
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Checks if the passed enumerable is null or empty
+        /// </summary>
+        /// <returns>True if the enumerable is null or does not contain any elements, otherwise false</returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable is null || !enumerable.Any();
+        }
     }
 }
